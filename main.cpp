f408e35ef3290bff3fff9d0f59d67b4d0dfa8f4e
JustSkill7562 <73040585+JustSkill7562@users.noreply.github.com>
2021-09-01 19:06:01 +0200

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,47 +1,21 @@
 #include <iostream>
-#include <string>
 using namespace std;
 
-void Run();
-void CheckOp();
-
-int number1;
-int number2;
-int result;
+double number1, number2, result;
 char op;
-
-bool running = false;
-
-void Run()
-{
-    cout << "Hello to this Calculator!" << endl;
-    cout << "Please type the first number" << endl;
-    cin >> number1;
-    cout << "Please type the second number" << endl;
-    cin >> number2;
-    cout << "Please type your Operator" << endl;
-    cin >> op;
-    CheckOp();
-    cout << "The Result of " << number1 << " + " << number2 << " = " << result << endl;
-}
-
-void CheckOp()
-{
-    if(op =='+') {result = number1 + number2;}
-    if(op =='-') {result = number1 - number2;}
-    if(op =='*') {result = number1 * number2;}
-    if(op =='/') {result = number1 / number2;}
-    if(op!= '+' && op != '-' && op != '*' && op != '/') {cout << "Wrong Operator!!!!!!" << endl;running = false;}
-}
 
 int main()
 {
-    running = true;
-    while (running)
-    {
-        Run();
-    }
-    cout << "Something went Wrong please close the Program an restart it ; )" << endl;
-    while(1){}
-    return 0;
+	cout << "Enter first Number" << endl;
+	cin >> number1;
+	cout << "Enter Second Number" << endl;
+	cin >> number2;
+	cout << "Enter an Operator" << endl;
+	cin >> op;
+	if (op == '+') { result = number1 + number2; cout << result << endl; }
+	else if (op == '-') { result = number1 - number2; cout << result << endl; }
+	else if (op == '*') { result = number1 * number2; cout << result << endl; }
+	else if (op == '/') { result = number1 / number2; cout << result << endl; }
+	else
+		cout << "Wrong Opertor" << endl;
 }
